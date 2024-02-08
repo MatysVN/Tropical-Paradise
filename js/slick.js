a9cf7817c03a2af72051a2b79ccdc2a8cc2f7477
Mateusz <mateuszskc@gmail.com>
2024-02-08 13:42:15 +0100

    added section contact

--- js/slick.js
+++ js/slick.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: true,
-        // Img change speed
+		// Img change speed
 		autoplaySpeed: 2500,
 		arrows: false,
 		mobileFirst: true,
-        // Media queries
+		// Media queries
 		responsive: [
 			{
 				breakpoint: 700,
